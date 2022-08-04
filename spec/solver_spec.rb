b61af13cbdb04f61a338ea2691d45c3832531e46
Pedro Marques <ppmarq1@gmail.com>
2022-08-04 10:24:54 +0100

    Add all factorial setup

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,9 +1,27 @@
 require './solver'
 
 describe Solver do
-  it "The factorial method returns the factorial value of a given number greater or equal 0" do
-    result = factorial(5)
+  it 'The factorial method returns the factorial value of a number greater than 0' do
+    result = Solver.new.factorial 6
 
-    expect(result).to eq 120
+    expect(result).to eq 720
+  end
+
+  it 'The factorial method returns the factorial value of a number equal to 0' do
+    result = Solver.new.factorial 0
+
+    expect(result).to eq 1
+  end
+
+  it 'The factorial method returns the factorial value of a number equal to 1' do
+    result = Solver.new.factorial 1
+
+    expect(result).to eq 1
+  end
+
+  it 'The factorial method returns the factorial value of a number less than 0' do
+    result = Solver.new.factorial(-5)
+
+    expect(result).to eq 'The number must be greater or equal to 0!'
   end
 end
