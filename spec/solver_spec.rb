8ed461981c6794299014a499ceb9729889682a95
Pedro Marques <ppmarq1@gmail.com>
2022-08-04 11:22:56 +0100

    Add reverse method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -5,32 +5,33 @@ describe Solver do
     it 'The factorial method returns the factorial value of a number greater than 0' do
       result = Solver.new.factorial 6
 
-    expect(result).to eq 720
+      expect(result).to eq 720
+    end
+
+    it 'The factorial method returns the factorial value of a number equal to 0' do
+      result = Solver.new.factorial 0
+
+      expect(result).to eq 1
+    end
+
+    it 'The factorial method returns the factorial value of a number equal to 1' do
+      result = Solver.new.factorial 1
+
+      expect(result).to eq 1
+    end
+
+    it 'The factorial method returns the factorial value of a number less than 0' do
+      result = Solver.new.factorial(-5)
+
+      expect(result).to eq 'The number must be greater or equal to 0!'
+    end
   end
 
-  it 'The factorial method returns the factorial value of a number equal to 0' do
-    result = Solver.new.factorial 0
+  context 'Reverse testing' do
+    it 'The reverse method returns the given string in a reversed form' do
+      result = Solver.new.reverse 'hello'
 
-    expect(result).to eq 1
-  end
-
-  it 'The factorial method returns the factorial value of a number equal to 1' do
-    result = Solver.new.factorial 1
-
-    expect(result).to eq 1
-  end
-
-  it 'The factorial method returns the factorial value of a number less than 0' do
-    result = Solver.new.factorial(-5)
-
-    expect(result).to eq 'The number must be greater or equal to 0!'
-  end
-end
-
-context 'Reverse testing' do
-  it 'The reverse method returns the given string in a reversed form' do
-    result = Solver.new.reverse 'hello'
-
-    expect(result).to eq 'olleh'
+      expect(result).to eq 'olleh'
+    end
   end
 end
